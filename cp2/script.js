881fdf9c482ab2b3d04d11aa6a546eaace981fc3
Kyle March <kcmarch23@gmail.com>
2018-02-14 21:49:50 -0700

    added in the rest of the JavaScript

---
+++ cp2/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	$("#quoteNumSubmit").click(function(e){
+		e.preventDefault();
+		var quoteNumber = $("#quote-number").find(":selected").text();
+
+		console.log(quoteNumber);
+		var myURL = "";
+		if(quoteNumber == 1){
+			myURL = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
+		} else{
+			myURL = "http://ron-swanson-quotes.herokuapp.com/v2/quotes/" + quoteNumber;
+		}
+
+		$.ajax({
+			url : myURL,
+			dataType : "json",
+			success : function(json){
+				console.log(json);
+				console.log("test");
+				var results = "";
+				for(var i = 0; i < json.length; i++){
+					console.log("for loop");
+					results += '<div class="quote-div">';
+					results += '<img src="./images/swanson-point.png">'
+					results += '<p>' + json[i]; + '</p>';
+					results += '</div>'
+				}
+				console.log("Results: " + results);
+				$("#returned-quotes").html(results);
+			},
+			error : function(){
+				var errorMessage = "";
+				errorMessage +=  '<h2>Error in getting Quotes</h2>';
+				$("#returned-quotes").html(errorMessage);
+			}
+		})
+	});
+});
